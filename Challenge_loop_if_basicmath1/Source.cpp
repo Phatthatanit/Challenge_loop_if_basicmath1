55ab8aa047c90efd146386b970c0afb37750ff3f
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-08-10 14:41:40 +0700

    daer1

--- Challenge_loop_if_basicmath1/Source.cpp
+++ Challenge_loop_if_basicmath1/Source.cpp
@@ -1,7 +1,28 @@
 #include<stdio.h>
+#include<math.h>
+
 int main() {
-	int a;
-	scanf_s("%d", &a);
-	printf("%d", a);
+	int a, b, i, x = 0;
+		float y=0,z=0;
+	scanf_s("%d %d", &a,&b);
+	//printf("%d %d", a,b);
+	if (a < b) {
+		for (i == b; a <= b; a++) {
+			printf("%d  ", a);
+			x += a;
+			y += 1;
+		}
+		printf("\nAverage = %.1f", x/y);
+	}
+	else if (a > b) {
+		for (i == a; b <= a; b++) {
+			printf("%d  ", b);
+			x += b;
+			y += 1;
+		}
+		printf("\nAverage = %.1f", x/y);
+		
+	}
+
 	return 0;
 }
